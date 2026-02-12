f7881d0fe2ac1ca31acb02d8eeb3e8eba6b1f30b
Shivam Kajaria <shivamkajaria@gmail.com>
2026-02-12 00:42:53 -0500

    migration fix

--- api/drizzle.config.js
+++ api/drizzle.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "drizzle-kit";
 
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
